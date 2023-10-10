153252b3cfcd18fec53bcc6e84f0f47327d9e582
gusteugenio <gustavoeugenio297@gmail.com>
2023-10-10 16:24:21 -0300

    Adicionando lógica de expansão do resumo (ts)

--- ts/src/app/app.component.ts
+++ ts/src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DevChuva';
+
+  readMore() {
+    const dots = document.getElementById('dots');
+    const more = document.getElementById('more');
+    const readMoreBtn = document.querySelector('.btn-show-more');
+
+    if (dots && more && readMoreBtn)
+      if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        more.style.display = "none"
+        readMoreBtn.innerHTML = "ver mais"
+      } else {
+        dots.style.display = "none";
+        more.style.display = "inline"
+        readMoreBtn.innerHTML = "ver menos"
+      }
+  }
 }
