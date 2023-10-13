90d0a11cf04da582cd9aa012a1ca3d433afca88b
gusteugenio <gustavoeugenio297@gmail.com>
2023-10-12 23:47:36 -0300

    Adicionando lógica de criar formulário e comments

--- ts/src/app/app.component.ts
+++ ts/src/app/app.component.ts
@@ -10,18 +10,46 @@ export class AppComponent {
 
   readMore() {
     const dots = document.getElementById('dots');
-    const more = document.getElementById('more');
-    const readMoreBtn = document.querySelector('.btn-show-more');
+    const more = document.getElementById('more'); 
+    const readMoreBtn = document.querySelector('.btn-show-more'); 
 
-    if (dots && more && readMoreBtn)
-      if (dots.style.display === "none") {
-        dots.style.display = "inline";
-        more.style.display = "none"
-        readMoreBtn.innerHTML = "ver mais"
-      } else {
-        dots.style.display = "none";
-        more.style.display = "inline"
-        readMoreBtn.innerHTML = "ver menos"
-      }
-  }
+    // Verifica se todos os elementos foram encontrados
+    if (dots && more && readMoreBtn) {
+        // Verifica se os pontos suspensivos estão visíveis
+        if (dots.style.display === "none") {
+            // Se estiverem invisíveis, se tornarão visíveis e o conteúdo adicional será ocultado
+            dots.style.display = "inline";
+            more.style.display = "none";
+            // Atualiza o texto do botão
+            readMoreBtn.innerHTML = "ver mais";
+        } else {
+            // Se estiverem visíveis, se tornarão invisíveis e o conteúdo adicional será exibido
+            dots.style.display = "none";
+            more.style.display = "inline";
+            // Atualiza o texto do botão
+            readMoreBtn.innerHTML = "ver menos";
+        }
+    }
+}
+
+
+  createTopic() {
+    const createTopicForm = document.querySelector('form') as HTMLFormElement | null;
+    const division = document.querySelector('.division') as HTMLElement | null;
+    
+    // Verifica se todos os elementos foram encontrados
+    if (createTopicForm && division) {
+        // Mostra o formulário
+        createTopicForm.style.display = 'block';
+    
+        // Oculta elementos
+        const elementsToHide = document.querySelectorAll('.btn-create-topic, .create-topic, #add');
+        elementsToHide.forEach(function(element) {
+            (element as HTMLElement).style.display = 'none';
+        });
+    
+        // Ajusta a margem superior
+        division.style.marginTop = '0';
+    }
+  }    
 }
