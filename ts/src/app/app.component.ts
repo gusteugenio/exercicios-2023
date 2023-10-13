99c66461b6638a7c309c61b4d2cf1b00b93321df
gusteugenio <gustavoeugenio297@gmail.com>
2023-10-13 11:00:19 -0300

    Adicionando lógica do sendTopic( ) e ajustes

--- ts/src/app/app.component.ts
+++ ts/src/app/app.component.ts
@@ -43,13 +43,37 @@ export class AppComponent {
         createTopicForm.style.display = 'block';
     
         // Oculta elementos
-        const elementsToHide = document.querySelectorAll('.btn-create-topic, .create-topic, #add');
+        const elementsToHide = document.querySelectorAll('.btn-create-topic, .create-topic, #add, .message-topics');
         elementsToHide.forEach(function(element) {
             (element as HTMLElement).style.display = 'none';
         });
     
         // Ajusta a margem superior
         division.style.marginTop = '0';
+
+        // Ajusta o display do division para quando a função for chamada no sendTopic
+        division.style.display = 'flex;'
     }
-  }    
+  }
+
+  sendTopic() {
+    const messageTopics = document.querySelector('.message-topics') as HTMLElement | null;
+    const form = document.querySelector('form') as HTMLFormElement | null
+    const division = document.querySelector('.division') as HTMLElement | null;
+  
+    // Verifica se todos os elementos foram encontrados
+    if (messageTopics && form && division) {
+  
+      // Mostra a mensagem de formulário submetido
+      messageTopics.style.display = 'block';
+      
+      // Oculta o formulário
+      form.style.display = 'none';
+
+      
+      // Oculta o division da tela base
+      division.style.display = 'none';
+
+    }
+  }
 }
